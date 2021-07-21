8370c5148c659046757661e0a100e42b1677aad3
MichaelYin1994 <zhuoyin94@163.com>
2021-07-22 03:06:54 +0800

    (feat) 重组了劣化的pipeline

--- input_pipeline.py
+++ input_pipeline.py
@@ -9,28 +9,29 @@
 本模块(input_pipeline.py)构建数据读取与预处理的pipline，并训练神经网络模型。
 '''
 
-import os
 import multiprocessing as mp
+import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 from tensorflow import keras
 from tensorflow.keras import Model, layers
 from tensorflow.keras.optimizers import Adam
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+from tqdm import tqdm
 
 from dingtalk_remote_monitor import RemoteMonitorDingTalk
 from models import build_model_resnet50_v2, build_model_resnet101_v2
 
 GLOBAL_RANDOM_SEED = 65535
-np.random.seed(GLOBAL_RANDOM_SEED)
-tf.random.set_seed(GLOBAL_RANDOM_SEED)
+# np.random.seed(GLOBAL_RANDOM_SEED)
+# tf.random.set_seed(GLOBAL_RANDOM_SEED)
 
 TASK_NAME = 'iccv_meituan_2021'
-GPU_ID = 1
+GPU_ID = 0
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
@@ -47,7 +48,6 @@ if gpus:
         print(e)
 # ----------------------------------------------------------------------------
 
-
 def build_efficentnet_model(verbose=False, is_compile=True, **kwargs):
     '''构造基于imagenet预训练的ResNetV2的模型，并返回编译过的模型。'''
 
@@ -56,22 +56,43 @@ def build_efficentnet_model(verbose=False, is_compile=True, **kwargs):
     input_shape = kwargs.pop('input_shape', (None, 224, 224))
     n_classes = kwargs.pop('n_classes', 1000)
 
+    model_name = kwargs.pop('model_name', 'EfficentNetB0')
+    model_lr = kwargs.pop('model_lr', 0.01)
+    model_label_smoothing = kwargs.pop('model_label_smoothing', 0.1)
+
+    # 依据关键字，构建模型
+    # ---------------------
     model = tf.keras.Sequential()
-    # initialize the model with input shape
+
+    if 'B0' in model_name:
+        model_tmp = tf.keras.applications.EfficientNetB0
+    elif 'B1' in model_name:
+        model_tmp = tf.keras.applications.EfficientNetB1
+    elif 'B2' in model_name:
+        model_tmp = tf.keras.applications.EfficientNetB2
+    elif 'B3' in model_name:
+        model_tmp = tf.keras.applications.EfficientNetB3
+    elif 'B4' in model_name:
+        model_tmp = tf.keras.applications.EfficientNetB4
+    elif 'B5' in model_name:
+        model_tmp = tf.keras.applications.EfficientNetB5
+    elif 'B6' in model_name:
+        model_tmp = tf.keras.applications.EfficientNetB6
+    elif 'B7' in model_name:
+        model_tmp = tf.keras.applications.EfficientNetB7
+
     model.add(
-        tf.keras.applications.EfficientNetB3(
+        model_tmp(
             input_shape=input_shape, 
             include_top=False,
             weights='imagenet',
-            drop_connect_rate=0.5,
+            drop_connect_rate=0.4,
         )
     )
     model.add(tf.keras.layers.GlobalAveragePooling2D())
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(
-        256,
-        activation='relu', 
-        bias_regularizer=tf.keras.regularizers.L1L2(l1=0.01, l2=0.001)
+        256, activation='relu',
     ))
     model.add(tf.keras.layers.Dropout(0.5))
     model.add(tf.keras.layers.Dense(n_classes, activation='softmax'))
@@ -83,45 +104,47 @@ def build_efficentnet_model(verbose=False, is_compile=True, **kwargs):
 
     if is_compile:
         model.compile(
-            loss='categorical_crossentropy',
-            optimizer=Adam(0.005),
+            loss=tf.keras.losses.CategoricalCrossentropy(
+                label_smoothing=model_label_smoothing),
+            optimizer=Adam(model_lr),
             metrics=['acc'])
 
     return model
 
 
-def load_preprocess_train_image(image_size=None):
-    '''通过闭包实现参数化的训练集的Image loading。'''
+def load_preprocessing_img(image_size, stage):
+    '''通过闭包实现参数化的Image Loading与TTA数据增强。'''
+    if stage not in ['train', 'valid', 'test']:
+        raise ValueError('stage must be either train, valid or test !')
 
-    def load_img(path=None):
-        image = tf.io.read_file(path)
-        image = tf.cond(
-            tf.image.is_jpeg(image),
-            lambda: tf.image.decode_jpeg(image, channels=3),
-            lambda: tf.image.decode_gif(image)[0])
+    if stage is 'train' or stage is 'test':
+        def load_img(path=None):
+            image = tf.io.read_file(path)
+            image = tf.cond(
+                tf.image.is_jpeg(image),
+                lambda: tf.image.decode_jpeg(image, channels=3),
+                lambda: tf.image.decode_gif(image)[0])
 
-        image = tf.image.random_brightness(image, 0.2)
-        image = tf.image.random_flip_left_right(image)
-        image = tf.image.random_flip_up_down(image)
+            image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+            image = tf.image.random_hue(image, max_delta=0.2)
+            image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+            image = tf.image.random_brightness(image, 0.3)
 
-        image = tf.image.resize(image, image_size)
-        return image
+            image = tf.image.random_flip_left_right(image)
+            image = tf.image.random_flip_up_down(image)
 
-    return load_img
+            image = tf.image.resize(image, image_size)
+            return image
+    else:
+        def load_img(path=None):
+            image = tf.io.read_file(path)
+            image = tf.cond(
+                tf.image.is_jpeg(image),
+                lambda: tf.image.decode_jpeg(image, channels=3),
+                lambda: tf.image.decode_gif(image)[0])
 
-
-def load_preprocess_test_image(image_size=None):
-    '''通过闭包实现参数化的测试集的Image loading。'''
-
-    def load_img(path=None):
-        image = tf.io.read_file(path)
-        image = tf.cond(
-            tf.image.is_jpeg(image),
-            lambda: tf.image.decode_jpeg(image, channels=3),
-            lambda: tf.image.decode_gif(image)[0])
-
-        image = tf.image.resize(image, image_size)
-        return image
+            image = tf.image.resize(image, image_size)
+            return image
 
     return load_img
 
@@ -129,20 +152,23 @@ def load_preprocess_test_image(image_size=None):
 if __name__ == '__main__':
     # 全局化的参数列表
     # ---------------------
-    IMAGE_SIZE = (512, 512)
-    BATCH_SIZE = 16
+    IMAGE_SIZE = (224, 224)
+    BATCH_SIZE = 32
     NUM_EPOCHS = 128
-    # NUM_WARMUP_EPOCHS = 6
-    # NUM_HOLD_EPOCHS = 5
-    EARLY_STOP_ROUNDS = 10
-    MODEL_NAME = 'EfficentNetB3_rtx3090'
+    EARLY_STOP_ROUNDS = 5
+    TTA_ROUNDS = 5
+
+    MODEL_NAME = 'EfficentNetB0_rtx3090'
+    MODEL_LR = 0.00007
+    MODEL_LABEL_SMOOTHING = 0
 
     CKPT_DIR = './ckpt/'
     CKPT_FOLD_NAME = '{}_GPU_{}_{}'.format(TASK_NAME, GPU_ID, MODEL_NAME)
 
-    IS_TRAIN_FROM_CKPT = False
-    IS_SEND_MSG_TO_DINGTALK = True
     IS_DEBUG = False
+    IS_TRAIN_FROM_CKPT = False
+    IS_SEND_MSG_TO_DINGTALK = False
+    IS_RANDOM_VISUALIZING_PLOTS = False
 
     if IS_DEBUG:
         TRAIN_PATH = './data/Train_debug/'
@@ -187,8 +213,12 @@ if __name__ == '__main__':
         val_label_oht_array.reshape(-1, 1)
     )
 
-    processor_train_image = load_preprocess_train_image(image_size=IMAGE_SIZE)
-    processor_valid_image = load_preprocess_test_image(image_size=IMAGE_SIZE)
+    processor_train_image = load_preprocessing_img(
+        image_size=IMAGE_SIZE,
+        stage='train')
+    processor_valid_image = load_preprocessing_img(
+        image_size=IMAGE_SIZE,
+        stage='valid')
 
     # 构造训练集数据
     train_path_ds = tf.data.Dataset.from_tensor_slices(train_file_full_name_list)
@@ -249,16 +279,16 @@ if __name__ == '__main__':
                 factor=0.7,
                 patience=3,
                 min_lr=0.0000003),
-        RemoteMonitorDingTalk(
-            is_send_msg=IS_SEND_MSG_TO_DINGTALK,
-            model_name=CKPT_FOLD_NAME,
-            gpu_id=GPU_ID)
     ]
 
     # 训练模型
     model = build_efficentnet_model(
         n_classes=N_CLASSES,
-        input_shape=IMAGE_SIZE + (3,)
+        input_shape=IMAGE_SIZE + (3,),
+        network_type=MODEL_NAME,
+        model_name=MODEL_NAME,
+        model_lr=MODEL_LR,
+        model_label_smoothing=MODEL_LABEL_SMOOTHING,
     )
 
     # 如果模型名的ckpt文件夹不存在，创建该文件夹
@@ -286,26 +316,36 @@ if __name__ == '__main__':
         callbacks=callbacks
     )
 
-    # 生成预测结果
+    # 生成Test预测结果，并进行Top-1 Accuracy评估
     # ---------------------
     test_file_name_list = os.listdir(TEST_PATH)
+    test_file_name_list = \
+        sorted(test_file_name_list, key=lambda x: int(x.split('.')[0][1:]))
     test_file_fullname_list = [TEST_PATH + item for item in test_file_name_list]
 
     test_path_ds = tf.data.Dataset.from_tensor_slices(test_file_fullname_list)
-    processor_test_image = load_preprocess_test_image(image_size=IMAGE_SIZE)
+    processor_test_image = load_preprocessing_img(
+        image_size=IMAGE_SIZE, stage='test')
     test_ds = test_path_ds.map(
         processor_test_image,
         num_parallel_calls=mp.cpu_count()
     )
     test_ds = test_ds.batch(BATCH_SIZE)
     test_ds = test_ds.prefetch(buffer_size=int(BATCH_SIZE * 2))
-    test_pred_proba = model.predict(test_ds)
+
+    # TTA强化
+    test_pred_proba_list = []
+    for i in tqdm(range(TTA_ROUNDS)):
+        test_pred_proba_list.append(model.predict(test_ds))
+    test_pred_proba = np.mean(test_pred_proba_list, axis=0)
+    test_pred_label_list = np.argmax(test_pred_proba, axis=1)
 
     test_pred_df = pd.DataFrame(
         test_file_name_list,
         columns=['Id']
     )
-    test_pred_df['Predicted'] = np.argmax(test_pred_proba, axis=1)
+    test_pred_df['Predicted'] = test_pred_label_list
 
-    sub_file_name = str(len(os.listdir('./submissions'))) + '_sub.csv'
+    sub_file_name = str(len(os.listdir('./submissions')) + 1) + \
+        '_{}_sub.csv'.format(MODEL_NAME)
     test_pred_df.to_csv('./submissions/{}'.format(sub_file_name), index=False)
